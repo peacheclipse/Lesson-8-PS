0661877ec41c49947c0aa2473fd55a752d04fd71
22024937-Royce <134566979+22024937-Royce@users.noreply.github.com>
2023-07-19 16:47:42 +0800

    royce

--- Collection Page.js
+++ Collection Page.js
@@ -13,14 +13,14 @@
     var emailinput = document.getElementById('email');
     var email = emailinput.value;
 
-    // Regular expression to check the email format
+    
    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   }
 if (!emailRegex.test(email)) {
-  alert('Please enter a valid email address.'){
-  return false;
+  alert('Please enter a valid email address.')
+  return false
 
-  }
+  
 
 return true;
 }
